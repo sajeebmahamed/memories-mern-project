3d2d61a9fb8dcdc68bbbd80082a8b1f43afb095a
Sajeeb <sajeebahamed6@gmail.com>
2020-11-10 18:05:08 +0600

    createpost/get post route created

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,5 +1,27 @@
-const getPosts = (req, res) => {
-    res.send('working');
+const PostMessage = require("../models/postMessage")
+
+const getPosts = async (req, res) => {
+    try {
+        const postMessage = await PostMessage.find()
+        res.status(200).json(postMessage)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post)
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+    res.send('create post')
 }
 
-module.exports = getPosts
+module.exports = {
+    getPosts,
+    createPost
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const getPosts = require('../controller/posts');
+const { getPosts, createPost } = require('../controller/posts');
+// const getPosts = require('../controller/posts');
 
 
 const router = express.Router()
 
 router.get('/', getPosts)
+router.post('/', createPost)
 
 module.exports = router
