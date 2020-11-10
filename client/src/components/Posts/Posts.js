c83c7245f76585f6ba4f2a25ea9b0a94146843c1
Sajeeb <sajeebahamed6@gmail.com>
2020-11-11 01:40:15 +0600

    redux added

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles';
 const Posts = () => {
     const classes = useStyles();
+    const posts = useSelector((state) => state.posts)
+    console.log(posts);
     return (
         <div>
             <Post />
