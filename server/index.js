d56160ef1525b322dac638ec3180fb08d90205ed
Sajeeb <sajeebahamed6@gmail.com>
2020-11-13 05:54:32 +0600

    env created

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors');
 const postRoutes = require('./routes/posts');
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express();
 
@@ -17,10 +20,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://sajeeb:sa112233@cluster0.i3zel.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
