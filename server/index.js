c6a5146a448dec5ca5c89e3fd2a4d0ce834083df
Sajeeb <sajeebahamed6@gmail.com>
2020-11-13 22:16:18 +0600

    deploy

--- server/index.js
+++ server/index.js
@@ -19,6 +19,9 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes)
+app.get('/', (req, res) => {
+    res.send('Memories API')
+})
 
 const PORT = process.env.PORT || 5000;
 
